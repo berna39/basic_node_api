9fa2f1cf48fa0bda19953ff1551e3a5fdfc56ffc
berna39 <kalemajoseph39@gmail.com>
2021-10-03 12:27:55 +0200

    Second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/test', (req,resp, next) => {
     console.log(req.query);
     console.log(req.body);
-    resp.send({ "feedback": "okey" })
+    resp.send({ "feedback": "okey by here" })
 })
 
 app.listen(5000, (err) => {
